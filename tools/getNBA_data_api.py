d3790e237650460de9de6afdbbafe2832a6e090d
Dinh Huyen <hd052437@edu.taitotalo.fi>
2025-11-06 14:44:21 +0200

    Add LogisticRegression

---
+++ tools/getNBA_data_api.py
@@ -0,0 +1,26 @@
+import requests as r
+import pandas as pd
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'Origin': 'https://www.nba.com',
+    'Referer': 'https://www.nba.com/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    }
+url = "https://stats.nba.com/stats/leaguedashteamstats?Conference=&DateFrom=&DateTo=&Division=&GameScope=&GameSegment=&Height=&ISTRound=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season={season}&SeasonSegment=&SeasonType=Regular%20Season&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision="
+
+def get_df_with_api(season_str):
+    season_url = url.format(season=season_str)
+    response = r.get(season_url, headers=headers, timeout=5)
+
+    data = response.json()
+    resultsets = data["resultSets"][0]
+
+    cols = resultsets["headers"]
+    rowSet = resultsets["rowSet"]
+    #print(rowSet)
+    df = pd.DataFrame(rowSet, columns=cols)
+
+    return df
---
+++ tools/getNBA_data_webscraping.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+def getData(year):
+    url = 'https://www.nba.com/stats/teams/traditional?dir=A&sort=TEAM_NAME&Season=' + year
+   
+    try:
+        driver = webdriver.Chrome()
+        driver.get(url)
+        
+        wait = WebDriverWait(driver, 10)
+
+        ok_button = wait.until(EC.element_to_be_clickable((By.ID, 'onetrust-accept-btn-handler')))
+        ok_button.click()
+
+        table = driver.find_element(By.CLASS_NAME, "Crom_table__p1iZz") 
+
+        thead = table.find_elements(By.XPATH, '*')[0]
+        tbody = table.find_elements(By.XPATH, '*')[1]
+
+        cols = thead.find_element(By.XPATH, '*')
+        rows = tbody.find_elements(By.XPATH, '*')
+
+        colList = cols.text.split()
+        data = pd.DataFrame(columns = colList)
+
+        for row in rows:
+            lines = row.text.splitlines()
+            row0 = lines[0] # teamname
+            row1 = lines[1].split() # data
+            row1.insert(0,row0)        
+            data.loc[len(data.index)] = row1
+    finally:
+        driver.close()
+        return data
+   
